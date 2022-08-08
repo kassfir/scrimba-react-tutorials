7e021f907fde38354f847daea0ec261f07fc3b21
kassfir <kasparseglitisriga@gmail.com>
2022-08-08 18:12:37 +0300

    Nav tutorial after the explanation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import logo from '../assets/react.png'
 
 export default function Navbar() {
     return (
-        <nav className='navbar'>
+        <nav>
             {/* Not to self – React loads stuff from the public folder instead of the assets folder. For me, at least. */}
-            <img src="react.svg"  className="navbar-item"></img>
-            <h3 className="navbar-item project-name">ReactFacts</h3> 
-            <h4 className="navbar-item project-info">React Course - Project 1</h4>
+            <img src="react.svg" className='nav--logo' ></img>
+            <h3 className='nav--logo_text'>ReactFacts</h3> 
+            <h4 className='nav--title'>React Course - Project 1</h4>
         </nav>
     )
 }
