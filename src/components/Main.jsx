a149dd35e2b59ae8a89bbffbef12382cf1b8ec4e
kassfir <kasparseglitisriga@gmail.com>
2022-08-08 18:18:11 +0300

    main section tutorial before checking

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,15 @@
 export default function Main () {
     return (
-        <h1>Main goes here</h1>
+        <div className="main--main_content">
+            <h1 className="main--header">Fun facts about React</h1>
+            <ul>
+                <li className='main--list_item'>Was first released in 2013</li>
+                <li className='main--list_item'>Was originally created by Jordan Walke</li>
+                <li className='main--list_item'>Has well over 100K stars of GitHub</li>
+                <li className='main--list_item'>Is maintained by Facebook</li>
+                <li className='main--list_item'>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+        
     )
 }
