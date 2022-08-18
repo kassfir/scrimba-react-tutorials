a5db7c45c2592bb35d4f0cc9883ec3dd8dce2d73
kassfir <kasparseglitisriga@gmail.com>
2022-08-18 19:18:28 +0300

    clean start

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,8 @@
 import React from "react"
-import TravelBlogEntry from "./components/TravelBlogEntry"
-import Navbar from "./components/Navbar"
-import data from './data'
-
 
 function App() {
-
-  let travelBlogEntries = data.map((item) => {
-    return (<div>
-      <TravelBlogEntry 
-        key={item.id}
-        item={item}
-      />
-      <hr className="blog-entry--separator"/>
-    </div>
-    );
-  })
-
-  console.log(travelBlogEntries);
-
-
   return (
-    <div>
-      <Navbar />
-      <div className="main-container">
-        { travelBlogEntries }
-      </div>
-    </div>
+    <h1>haha memes</h1>
   )
 }
 
--- src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEarthAfrica } from '@fortawesome/free-solid-svg-icons'
-
-export default function Navbar () {
-    return <nav>
-        <FontAwesomeIcon icon={faEarthAfrica} className='nav--icon'/>
-        <h3 className='nav--header'>my travel journal</h3>
-    </nav>
-}
