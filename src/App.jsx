726b82c54f3e1b1c1bd8a52862aa8864d2ea131a
kassfir <kasparseglitisriga@gmail.com>
2022-09-24 17:29:10 +0300

    Implemented high score system that holds data in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,24 @@ import Confetti from 'react-confetti';
 
 function App() {
 
+  const [startTime, setStartTime] = React.useState(() => Date.now());
+  const [elapsedTime, setElapsedTime] = React.useState(() => Date.now());
+  const [rollCount, setRollCount] = React.useState(() => 0);
   const [dice, setDice] = React.useState(() => allNewDice());
-  const [ tenzies, setTenzies ] = React.useState(false);
+  const [tenzies, setTenzies ] = React.useState(false);
+  const [isTimeHighScore, setIsTimeHighScore ] = React.useState(false);
+  const [isRollHighScore, setIsRollHighScore ] = React.useState(false);
 
   React.useEffect(() => {
     const checkedValue = dice[0].value;
-    const allSameValue = dice.every(die => die.value === checkedValue);
+    const allSameValue = dice.every(die => die.value === checkedValue && die.isHeld);
 
     if (allSameValue) {
+      setElapsedTime(Date.now() - startTime);
       setTenzies(true);
-      console.log('all are equal');
+      checkRollCountHighScore();
+      checkElapsedTimeHighScore();
     }
-
   }, [dice])
 
   function allNewDice() {
@@ -42,6 +48,7 @@ function App() {
   }
 
   function rollDice() {
+    setRollCount(prevCount => prevCount + 1)
     setDice(prevDice => {
       const newDice = [];
 
@@ -66,24 +73,107 @@ function App() {
   function restartGame () {
     setDice(allNewDice());
     setTenzies(false);
+    setStartTime(Date.now());
+    setRollCount(0);
   }
 
+  function clearScore (event) {
+    event.preventDefault(); 
+    localStorage.clear()
+  }
+
+  function checkRollCountHighScore () {
+
+    const highScoreString = localStorage.getItem('rollCount');
+
+    if (!highScoreString) {
+      localStorage.setItem('rollCount', rollCount.toString());
+      setIsRollHighScore(true);
+      return true;
+    }
+
+    const highScore = parseInt(highScoreString);
+
+    if (highScore > rollCount) { 
+      localStorage.setItem('rollCount', rollCount.toString());
+      setIsRollHighScore(true);
+      return true;
+    }
+
+    setIsRollHighScore(false);
+    return false;
+  }
+  
+  function checkElapsedTimeHighScore () {
+
+    const highScoreString = localStorage.getItem('elapsedTime');
+
+    if (!highScoreString) {
+      localStorage.setItem('elapsedTime', elapsedTime.toString());
+      setIsTimeHighScore(true);
+      return true;
+    }
+
+    const highScore = parseInt(highScoreString);
+
+    if (highScore > elapsedTime) { 
+      localStorage.setItem('elapsedTime', elapsedTime.toString());
+      setIsTimeHighScore(true);
+      return true;
+    }
+
+    setIsTimeHighScore(false);
+    return false;
+  }
+
+
   const mainButton = tenzies ? 
-    <button className="reroll-button" onClick={restartGame}>New game</button>:
-    <button className="reroll-button" onClick={rollDice}>Roll</button>;
+    <button className="button reroll-button" onClick={restartGame}>New game</button>:
+    <button className="button reroll-button" onClick={rollDice}>Roll</button>;
+
+  const clearScoreButton = <button className="button clear-score-button" onClick={clearScore}>Clear high score</button>
 
 
-  const diceElements = dice.map((die) => <Die clickHandler={() => hold(die.id)} isHeld={die.isHeld} number={die.value} key={die.id}/>)
+  const diceElements = dice.map((die) => 
+    <Die clickHandler={() => { 
+        if (!tenzies) {
+          hold(die.id)
+        }
+      }
+    } 
+    isHeld={die.isHeld} 
+    number={die.value} 
+    key={die.id}
+  />)
+
+  
+  function newTimeRecord () {
+    return (tenzies && isTimeHighScore && <span className="record-announcement">Best time!</span> )
+  } 
+  
+  function newRollRecord () {
+    return (tenzies && isRollHighScore && <span className="record-announcement">Least rolls!</span> )
+  } 
+  
+  const score = <p className="instructions">It took you {rollCount} rolls {newRollRecord()}<br/>and {elapsedTime/1000} seconds {newTimeRecord()}<br/>to win!</p>
+
+  
 
   return (
     <main>
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+ 
       <div className='dice-container'>
         {diceElements}
       </div>
       { mainButton }
+      {tenzies && score}
+      { tenzies && isTimeHighScore && newTimeRecord}
+      { tenzies && isRollHighScore && newRollRecord}
+      { tenzies && clearScoreButton}
       {tenzies && <Confetti />}
+
     </main>
   )
 }
