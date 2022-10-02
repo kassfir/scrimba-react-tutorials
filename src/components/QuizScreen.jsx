bea87d47e0a40078bb713bc5dd2b84e5572e031c
kassfir <kasparseglitisriga@gmail.com>
2022-10-02 18:24:41 +0300

    Added support for single question + handleSelectAnswer + some styling

--- src/components/QuizScreen.jsx
+++ src/components/QuizScreen.jsx
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 import React, {useEffect, useState} from 'react';
+import Question from './Question';
 
 
 function QuizScreen () {
@@ -11,25 +12,90 @@ function QuizScreen () {
         fetch('https://opentdb.com/api.php?amount=5')
             .then(responseData => responseData.json())
             .then(responseJson => {
-                
                 const quiz = [];
-                responseJson.results.forEach(question => {
-                    quiz.push({
-                        ...question,
-                        id: nanoid(),
-                        userSelected: '',
-                    });
-                })
 
-                setQuizQuestions(quiz);
-                console.log(quizQuestions);
+                responseJson.results.forEach(question => {
+                    const incorrectAnswers = [];
+
+                    for (let i = 0; i < question.incorrect_answers.length; i++){
+                        incorrectAnswers.push(
+                            {
+                                answerText: question.incorrect_answers[i],
+                                isCorrect: false,
+                                isSelected: false,
+                                id: nanoid(),
+                            }
+                        );
+                    }
+
+                    const correctAnswer = {
+                        answerText: question.correct_answer,
+                        isCorrect: true,
+                        isSelected: false,
+                        id: nanoid(),
+                    }
+
+                    quiz.push({
+                        id: nanoid(),
+                        question: question.question,
+                        answers: [
+                            correctAnswer,
+                            ...incorrectAnswers
+                        ],
+                    });
+
+                    setQuizQuestions(quiz);
+                    setIsLoaded(true);
+                })
             })
             .catch(error => console.error(error))
-        }, [])
+        }, 
+    []);
+
+    function handleAnswerSelect (event, questionId, answerId){
+        event.preventDefault();
+
+        setQuizQuestions(prevState => {
+            return prevState.map(prevStateQuestion => {
+                if (questionId !== prevStateQuestion.id) {
+                    return prevStateQuestion;
+                }
+
+                const updatedAnswers = prevStateQuestion.answers.map(prevAnswer => {
+                    const isSelected = prevAnswer.id === answerId ? true : false;
+
+                    return ({
+                        ...prevAnswer, 
+                        isSelected: isSelected,
+                    })
+                })
+
+                console.log(prevStateQuestion);
+
+                return ({
+                    ...prevStateQuestion, 
+                    answers: updatedAnswers,
+                });
+            })
+
+        });
+
+        console.log(quizQuestions);
+    }
+
+    
+
+    
+
 
     return (
-        
-        <h1>Quiz screen</h1>
+        isLoaded ? 
+        <Question 
+            key={quizQuestions[0].id} 
+            question={quizQuestions[0]} 
+            handleAnswerSelect={handleAnswerSelect} 
+        /> :
+        <h1>...loading!</h1>
     );
 }
 
